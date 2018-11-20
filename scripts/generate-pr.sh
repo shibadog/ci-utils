2e5838b1913f0d548aa91ec43da6b0fb5d67850c
Toshiaki Maki <tmaki@pivotal.io>
2018-11-21 02:52:46 +0900

    Add generate-pr.sh

---
+++ scripts/generate-pr.sh
@@ -0,0 +1,43 @@
+# https://github.com/cloudfoundry-incubator/kubo-ci/blob/master/scripts/lib/generate-pr.sh
+
+create_pr_payload() {
+  title="$1 upgrade $2"
+  body="This is an auto generated PR created for $1 upgrade to $2"
+  echo '{"title":"'"$title"'","body":"'"$body"'","head":"'"$3"'","base":"develop"}'
+}
+
+# Needs to be called from the directory where PR needs to be generated
+generate_pull_request() {
+  local component=$1
+  local tag=$2
+  local repo=$3
+
+  mkdir -p ~/.ssh
+  cat > ~/.ssh/config <<EOF
+StrictHostKeyChecking no
+LogLevel quiet
+EOF
+  chmod 0600 ~/.ssh/config
+
+  cat > ~/.ssh/id_rsa <<EOF
+${GIT_SSH_KEY}
+EOF
+  chmod 0600 ~/.ssh/id_rsa
+  eval $(ssh-agent) >/dev/null 2>&1
+  trap "kill $SSH_AGENT_PID" 0
+  ssh-add ~/.ssh/id_rsa
+
+  git config --global user.email "${GIT_EMAIL}"
+  git config --global user.name "${GIT_NAME}"
+
+  branch_name="upgrade/${component}${tag}"
+  git checkout -b $branch_name
+  git add .
+  git commit -m "Upgrade $component to $tag"
+  git push origin $branch_name
+
+  # create a PR here
+  token=${GITHUB_API_TOKEN}
+  payload=$(create_pr_payload $component $tag $branch_name)
+  curl -u making:${GITHUB_API_TOKEN} -H "Content-Type: application/json" -X POST -d "$payload" https://api.github.com/repos/making/${repo}/pulls
+}
