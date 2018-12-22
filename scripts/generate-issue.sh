1d2d6b74c70d90036e6eb7bf56cc8b482da61702
Toshiaki Maki <tmaki@pivotal.io>
2018-12-22 23:08:09 +0900

    add generate-issue.sh

---
+++ scripts/generate-issue.sh
@@ -0,0 +1,17 @@
+create_issue_payload() {
+  title="$1 upgrade $2"
+  body="This is an auto generated issue created for $1 upgrade to $2"
+  echo '{"title":"'"$title"'","body":"'"$body"'"}'
+}
+
+generate_issue() {
+  local user=$1
+  local component=$2
+  local tag=$3
+  local repo=$4
+
+  # See https://developer.github.com/v3/issues/#create-an-issue
+  token=${GITHUB_API_TOKEN}
+  payload=$(create_issue_payload $component $tag)
+  curl -u ${user}:${GITHUB_API_TOKEN} -H "Content-Type: application/json" -X POST -d "$payload" https://api.github.com/repos/${repo}/issues
+}
